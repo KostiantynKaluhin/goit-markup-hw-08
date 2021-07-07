af0bb82eb999847010b28d03c6bb1f14da78a737
Kostianyn <odessavshok@gmail.com>
2021-07-07 23:28:47 +0300

    Практически все готово, осталось точечно поправить
    Выполнил разметку и оформление дз8

--- js/menu.js
+++ js/menu.js
@@ -43,3 +43,17 @@
 //     mobileMenuRef.classList.toggle("is-open");
 //   });
 // })();
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.mobile-menu__open'),
+    closeMenuBtn: document.querySelector('.mobile-menu__close'),
+    menu: document.querySelector('.mobile-menu'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+  }
+})();
